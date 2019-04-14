a32f2b8a28e574227fbd2af2b37d5d99de3345ad
Scripter36 <1350adwx@gmail.com>
2019-04-15 01:41:45 +0900

    Create mergeFace need optimization

--- assembly/index.ts
+++ assembly/index.ts
@@ -2,20 +2,116 @@ import 'allocator/arena'
 // @ts-ignore
 export { memory }
 
-let chunk: u32[]
+declare namespace console {
+  // @ts-ignore 정말 없애버리고 싶지만 디버그 때문에 만듬
+  @external("console", "log")
+  export function log(s: i32): void
+}
+
+let chunk: Uint32Array
 
 function getBlock (x: u8, y: u8, z: u8): u32 {
   return chunk[x << 12 + z << 8 + y]
 }
 
-export function optimize (data: u32[]): u32[] {
+export function mergeFace (faces: Uint32Array, width: u16, height: u16): Uint32Array {
+  let startX: u8 = 0
+  let startY: u8 = 0
+  let searchX: u8 = 0
+  let searchY: u8 = 0
+  let searchingX: bool = true
+  let id: u32 = 0
+  let result = new Uint32Array(faces.length * 4)
+  let nextIndex = 0
+  for (let i: i32 = 0; i < faces.length; i++) {
+    if (faces[i] !== 0) {
+      id = faces[i]
+      startY = <u8> (i / width) | 0 // Force Integer
+      searchY = startY
+      startX = <u8> (i - startY * width)
+      searchX = startX
+      break
+    }
+  }
+  if (id === 0) return result
+  while (true) {
+    if (searchingX) {
+      if (searchX === width - 1) {
+        searchingX = false
+        continue
+      }
+      searchX++
+      if (faces[startY * width + searchX] !== id) {
+        searchX--
+        searchingX = false
+        continue
+      }
+    } else {
+      let found: bool = false
+      if (searchY === height - 1) {
+        found = true
+      }
+      searchY++
+      if (!found) {
+        for (let x: u8 = startX; x <= searchX; x++) {
+          if (faces[searchY * width + searchX] !== id) {
+            found = true
+            break
+          }
+        }
+      }
+      if (found) {
+        searchY--
+        result[nextIndex] = startX
+        result[nextIndex + 1] = startY
+        result[nextIndex + 2] = searchX
+        result[nextIndex + 3] = searchY
+        nextIndex += 4
+        let findX: u8 = startX
+        let findY: u8 = startY
+        while (true) {
+          if (findX === width - 1) {
+            findX = 0
+            if (findY === height - 1) {
+              findY = 0
+              break
+            }
+            findY++
+          } else {
+            findX++
+          }
+          let contains = false
+          for (let i: i32 = 0; i < result.length; i += 4) {
+            if (result[i] <= findX && findX <= result[i + 2] && result[i + 1] <= findY && findY <= result[i + 3]) {
+              contains = true
+              break
+            }
+          }
+          if (!contains && faces[findX + findY * width] !== 0) break
+        }
+        if (findX === 0 && findY === 0) {
+          break
+        } else {
+          startX = findX
+          searchX = findX
+          startY = findY
+          searchY = findY
+          id = faces[startX + startY * width]
+          searchingX = true
+        }
+      }
+    }
+  }
+  return result.subarray(0, nextIndex)
+}
+
+export function optimize (data: Uint32Array): Uint32Array {
   chunk = data
-  let mergedData: u32[] = []
   for (let x: u8 = 0; x <= 0; x++) {
+    let frontFace = new Uint32Array(4096)
+    let backFace = new Uint32Array(4096)
     for (let y: u8 = 0; y <= 255; y++) {
       for (let z: u8 = 0; z <= 15; z++) {
-        let frontFace: u32[] = []
-        let backFace: u32[] = []
         let block = getBlock(x, y, z)
         let frontBlock = x === 15 ? 0 : getBlock(x + 1, y, z)
         if (frontBlock === 0) {
@@ -27,6 +123,7 @@ export function optimize (data: u32[]): u32[] {
         }
       }
     }
+    return mergeFace(frontFace, 16, 256)
   }
-  return mergedData
+  return new Uint32Array(0)
 }
--- test/index.js
+++ test/index.js
@@ -4,17 +4,30 @@ const loader = require('assemblyscript/lib/loader')
 const fs = require('fs')
 
 const assemblyModule = loader.instantiateBuffer(fs.readFileSync('./build/untouched.wasm'), {
-  memoryBase: 0,
-  tableBase: 0,
-  memory: new WebAssembly.Memory({
-    initial: 256
-  }),
-  table: new WebAssembly.Table({
-    initial: 0,
-    element: 'anyfunc'
-  })
+  console: {
+    log: (msg) => {
+      console.log(`WASM >> ${msg}`);
+    }
+  },
+  env: {
+    memory: new WebAssembly.Memory({ initial: 256 }),
+    table: new WebAssembly.Table({ initial: 0, element: 'anyfunc' })
+  }
 })
 const array = new Uint32Array(65536)
-for (let i = 0; i < 65536; i++) array[i] = 1
+for (let i = 0; i < 65536; i++) array[i] = Math.floor(Math.random() * 3)
 const ptr = assemblyModule.newArray(array)
-console.log(assemblyModule.getArray(Uint32Array, assemblyModule.optimize(ptr)))
+const previous = Date.now()
+const result = assemblyModule.getArray(Uint32Array, assemblyModule.mergeFace(ptr, 256, 256))
+const next = Date.now()
+console.log(`estimated time: ${next - previous}ms`) // 65536 faces: 30038ms. too slow
+for (let i = 0; i < result.length; i += 4) {
+  console.log(`${result[i]}, ${result[i + 1]}, ${result[i + 2]}, ${result[i + 3]}`)
+}
+/**
+ * Result
+ * 0, 0, 3, 0
+ * 0, 1, 1, 1
+ * 2, 1, 3, 3
+ * 0, 2, 3, 3
+ */
